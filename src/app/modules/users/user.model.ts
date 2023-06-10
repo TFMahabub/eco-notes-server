b4d8638dd8fe066d5ecbdb6ac288eafa39889f51
Mahabub Alam <rjmahabub543@gmail.com>
2023-06-11 03:59:44 +0600

    user.interface, user.model done

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,31 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
+
+type UserModel = Model<IUser, object>
+
+const userSchema = new Schema<IUser>({
+  _id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  savedItem: {
+    type: [String],
+  },
+  createdAt: {
+    type: String,
+  },
+  updatedAt: {
+    type: String,
+  },
+})
+
+export const User = model<IUser, UserModel>('User', userSchema)
---
+++ src/app/modules/users/users.interface.ts
@@ -0,0 +1,8 @@
+export type IUser = {
+  _id: string
+  name: string
+  role: string
+  savedItem: string[]
+  createdAt: string
+  updatedAt: string
+}
