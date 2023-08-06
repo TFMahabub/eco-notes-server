994c90023517a4d04de80c8fd5436d27d8210655
Mahabub Alam <rjmahabub543@gmail.com>
2023-08-06 16:21:36 +0600

    [DONE] tags crud-opations

--- index.js
+++ index.js
@@ -37,10 +37,18 @@ async function porductCollection() {
       const result = await blogCollection.insertOne(product);
       res.send(result);
     });
-    app.get("/add_products", async (req, res) => {
-      const query = {};
-      const cursor = blogCollection.find(query);
-      const result = await cursor.toArray();
+    app.delete("/delete-blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await blogCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.put("/update-blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedDoc = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = { $set: updatedDoc };
+      const result = await blogCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
   } catch {
@@ -52,43 +60,96 @@ porductCollection().catch((err) =>
   console.error("This is out of the function product error: ", err)
 );
 
-//User Collection--------------------------------
-async function userCollection() {
-  const UserCollection = client.db("eco-notes-server").collection("user");
+
+
+//popular tags Collection-------------------------------
+async function popularTagsCollections() {
+  const TagsColletions = client.db("eco-notes-server").collection("popular-tags");
   try {
-    app.post("/user", async (req, res) => {
-      const user = req.body;
-      const result = await UserCollection.insertOne(user);
-      res.send(result);
+    app.get("/all-tags", async (req, res) => {
+      const query = {};
+      const cursor = TagsColletions.find(query);
+      const result = await cursor.toArray();
+      res.send(result.slice(0, 8));
     });
-    app.delete("/user/:id", async (req, res) => {
+    app.get("/single-tag/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await UserCollection.deleteOne(query);
+      const result = await TagsColletions.findOne(query);
       res.send(result);
     });
-    app.put("/user/:id", async (req, res) => {
+    app.delete("/delete-tag/:id", async (req, res) => {
       const id = req.params.id;
-      const updatedDoc = req.body;
-      const filter = { _id: ObjectId(id) };
-      const updateDoc = { $set: updatedDoc };
-      const result = await UserCollection.updateOne(filter, updateDoc);
+      const query = { _id: ObjectId(id) };
+      const result = await TagsColletions.deleteOne(query);
       res.send(result);
     });
-    app.get("/user", async (req, res) => {
-      const query = {};
-      const cursor = UserCollection.find(query);
-      const result = await cursor.toArray();
+    app.post("/post-tag", async (req, res) => {
+      const tag = req.body;
+      const result = await TagsColletions.insertOne(tag);
       res.send(result);
     });
   } catch {
-    (err) => console.error("this user error:", err);
+    (err) => console.error("this productCollection error:", err);
   } finally {
   }
 }
-userCollection().catch((err) =>
-  console.error("This is out of the function user error: ", err)
+popularTagsCollections().catch((err) =>
+  console.error("This is out of the function product error: ", err)
 );
+
+//User Collection--------------------------------
+// async function porductCollection() {
+//   const blogCollection = client.db("eco-notes-server").collection("users");
+//   try {
+//     app.get("/users", async (req, res) => {
+//       const query = {};
+//       const cursor = blogCollection.find(query);
+//       const result = await cursor.toArray();
+//       res.send(result);
+//     });
+//     app.get("/single-blog/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const query = { _id: ObjectId(id) };
+//       const result = await blogCollection.findOne(query);
+//       res.send(result);
+//     });
+//     app.post("/add-blog", async (req, res) => {
+//       const product = req.body;
+//       const result = await blogCollection.insertOne(product);
+//       res.send(result);
+//     });
+//     app.delete("/delete-blog/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const query = { _id: ObjectId(id) };
+//       const result = await blogCollection.deleteOne(query);
+//       res.send(result);
+//     });
+//     app.put("/update-blog/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const updatedDoc = req.body;
+//       const filter = { _id: ObjectId(id) };
+//       const updateDoc = { $set: updatedDoc };
+//       const result = await blogCollection.updateOne(filter, updateDoc);
+//       res.send(result);
+//     });
+//   } catch {
+//     (err) => console.error("this productCollection error:", err);
+//   } finally {
+//   }
+// }
+// porductCollection().catch((err) =>
+//   console.error("This is out of the function product error: ", err)
+// );
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
   res.send("wellcome to eco-notes");
