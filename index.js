644492bf17c0c49041447f5e03a52a2719aa756a
Mahabub Alam <rjmahabub543@gmail.com>
2023-08-11 07:07:32 +0600

    [DONE] searcing

--- index.js
+++ index.js
@@ -70,13 +70,36 @@ async function popularTagsCollections() {
       const query = {};
       const cursor = TagsColletions.find(query);
       const result = await cursor.toArray();
-      res.send(result.slice(0, 8));
+      res.send(result);
     });
     app.get("/single-tag/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await TagsColletions.findOne(query);
       res.send(result);
+    });
+    app.get("/isAlreadyHave-tag", async (req, res) => {
+      const tagName = req.query.searchText;
+      const query = {tag: tagName}
+      const result = await TagsColletions.findOne(query)
+      res.send(result)
+    });
+    app.get("/search-tag", async (req, res) => {
+      const searchText = req.query.t;
+      const result = await TagsColletions.aggregate([
+          {
+            $search: {
+              index: "search-text",
+              text: {
+                query: searchText,
+                path: "tag",
+                fuzzy: {},
+              }
+            }
+          }
+        ])
+        console.log(result);
+      res.send(result)
     });
     app.delete("/delete-tag/:id", async (req, res) => {
       const id = req.params.id;
@@ -98,39 +121,31 @@ popularTagsCollections().catch((err) =>
   console.error("This is out of the function product error: ", err)
 );
 
-//User Collection--------------------------------
-// async function porductCollection() {
-//   const blogCollection = client.db("eco-notes-server").collection("users");
+// //popular tags Collection-------------------------------
+// async function TagsColorCollections() {
+//   const TagColorColletions = client.db("eco-notes-server").collection("tag-colors");
 //   try {
-//     app.get("/users", async (req, res) => {
+//     app.get("/tag-colors", async (req, res) => {
 //       const query = {};
-//       const cursor = blogCollection.find(query);
+//       const cursor = TagColorColletions.find(query);
 //       const result = await cursor.toArray();
-//       res.send(result);
+//       res.send(result.slice(0, 8));
 //     });
-//     app.get("/single-blog/:id", async (req, res) => {
+//     app.get("/single-tag-color/:id", async (req, res) => {
 //       const id = req.params.id;
 //       const query = { _id: ObjectId(id) };
-//       const result = await blogCollection.findOne(query);
+//       const result = await TagColorColletions.findOne(query);
 //       res.send(result);
 //     });
-//     app.post("/add-blog", async (req, res) => {
-//       const product = req.body;
-//       const result = await blogCollection.insertOne(product);
-//       res.send(result);
-//     });
-//     app.delete("/delete-blog/:id", async (req, res) => {
+//     app.delete("/delete-single-tag-color/:id", async (req, res) => {
 //       const id = req.params.id;
 //       const query = { _id: ObjectId(id) };
-//       const result = await blogCollection.deleteOne(query);
+//       const result = await TagColorColletions.deleteOne(query);
 //       res.send(result);
 //     });
-//     app.put("/update-blog/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const updatedDoc = req.body;
-//       const filter = { _id: ObjectId(id) };
-//       const updateDoc = { $set: updatedDoc };
-//       const result = await blogCollection.updateOne(filter, updateDoc);
+//     app.post("/post-single-tag-color", async (req, res) => {
+//       const tag = req.body;
+//       const result = await TagColorColletions.insertOne(tag);
 //       res.send(result);
 //     });
 //   } catch {
@@ -138,9 +153,10 @@ popularTagsCollections().catch((err) =>
 //   } finally {
 //   }
 // }
-// porductCollection().catch((err) =>
+// TagsColorCollections().catch((err) =>
 //   console.error("This is out of the function product error: ", err)
 // );
+
 
 
 
